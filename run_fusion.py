f8e50aad655ec67479ff9bd29b1ade708db280a3
Ilan <bebetteryou2015@gmail.com>
2023-01-11 09:52:00 +0000

    Added functionality for making fusion a task now

--- run_fusion.py
+++ run_fusion.py
@@ -21,6 +21,8 @@ URL_PREFIX = "http://74.82.29.209:9000"
 CUR_FOLDER = os.path.dirname(os.path.abspath(__file__))
 INTERSECTION_THRESHOLD = 0.97
 CELEBRITY_PATH = os.path.join(os.path.join(CUR_FOLDER, "data"), "list_of_celebrities.txt")
+REID_COLLECTION_NAME = "s4_re_id"
+VISUAL_CLUES_COLLECTION_NAME = "s4_visual_clues"
 
 class FusionPipeline:
     def __init__(self):
@@ -29,9 +31,139 @@ class FusionPipeline:
         self.collection_name = "s4_fusion"
         self.celebrity_data = self.get_celebrity_data()
 
-
     def run_fusion_pipeline(self, movie_id):
-        return
+        print("Starting to record time of fusion task!")
+        start_time = time.time()
+
+        print("Working on Movie ID: {}".format(movie_id))
+
+        reid_detections = self.get_reid_detections(movie_id = movie_id, collection=REID_COLLECTION_NAME)
+        if not reid_detections:
+            print("ERROR!!! REID data was not found!")
+            return False, None
+        
+        fusion_output = {
+            'movie_id': movie_id,
+            'frame_numbers': {}
+        }
+        
+        data_for_db = {}
+
+        # Iterate over all the RE-ID frames.
+        for reid_detection in reid_detections:
+
+            reid_frame = reid_detection['frame_num']
+            vc_data = self.get_visual_clues_data(movie_id = movie_id, collection=VISUAL_CLUES_COLLECTION_NAME, frame_num=reid_frame)
+            if not vc_data:
+                print("ERROR!!! VISUAL CLUES DATA was not found!")
+                return False, None
+            vc_rois = self.get_visual_clues_rois(visual_clue_data=vc_data)
+                
+            reid_bboxes = reid_detection['re-id']
+            # Iterate over the RE-ID face(s) in the current frame (There may be multiple different Face IDs)
+            for reid_bbox_obj in reid_bboxes:
+                face_id = reid_bbox_obj['id']
+                reid_bbox = reid_bbox_obj['bbox']
+
+                # Iterate over Visual Clues bboxes for the specific face. (that have 'person' in them)
+                for vc_roi in vc_rois:
+                    vc_bbox = vc_roi['bbox']
+                    vc_bbox = vc_bbox.replace("[","").replace("]","").split(",")
+                    vc_bbox = [float(xy) for xy in vc_bbox]
+                    bboxes_intersection = self.calculate_intersection \
+                                            (reid_bbox=reid_bbox, vc_bbox=vc_bbox, movie_id=movie_id, frame_num=reid_frame)
+                    
+                    # Keep the bounding boxes that have high intersection
+                    if bboxes_intersection > INTERSECTION_THRESHOLD:
+                        # Save the information of strong candidates regrding bboxes intersection
+                        reid_frame_str = str(reid_frame)
+                        if reid_frame_str not in fusion_output['frame_numbers']:
+                            fusion_output['frame_numbers'][reid_frame_str] = {'intersections' : [], 'ious': []}
+                        
+                        face_area = bb_smallest_area(reid_bbox, vc_bbox)
+                        iou = bb_intersection_over_union(reid_bbox, vc_bbox)
+                        fusion_output['frame_numbers'][reid_frame_str]['intersections'].append(
+                            {
+                                'reid_bbox': reid_bbox,
+                                'vc_bbox': vc_bbox,
+                                'bbox_intersection': bboxes_intersection,
+                                'face_area': face_area,
+                                'face_id': str(face_id),
+                                'iou': iou,
+                                'vc_id': str(vc_roi['roi_id'])
+                            }
+                        )
+                        if bboxes_intersection == 1.0:
+                            fusion_output['frame_numbers'][reid_frame_str]['ious'].append(
+                                {
+                                    'iou': bboxes_intersection,
+                                    'face_id': str(face_id),
+                                    'vc_id': str(vc_roi['roi_id']),
+                                    'reid_bbox': reid_bbox,
+                                    'vc_bbox': vc_bbox
+                                }
+                            )
+
+        movie_id = fusion_output['movie_id']
+        frames = fusion_output['frame_numbers']
+        for frame_num, _ in frames.items():
+            
+            image_url = self.get_image_url(movie_id, frame_num=int(frame_num), collection=VISUAL_CLUES_COLLECTION_NAME)
+            movie_name = image_url.split("/")[-2]
+            print("Working on movie: {}, frame: {}".format(movie_name, frame_num))
+
+            matches = []
+            # Get all the matches for the current frame
+            for intersection in frames[str(frame_num)]['intersections']:
+                matches.append({
+                                'reid_bbox':            intersection['reid_bbox'],
+                                'vc_bbox':              intersection['vc_bbox'],
+                                'bbox_intersection':    intersection['bbox_intersection'],
+                                'face_area':            intersection['face_area'],
+                                'face_id':              intersection['face_id'],
+                                'vc_id':                intersection['vc_id']
+                            })
+            
+            post_processed_matches = self.correct_matches(matches)
+
+            vc_ids = self.get_visual_clues_person_ids(movie_id, int(frame_num), collection="s4_visual_clues")
+            face_ids = self.get_reid_face_ids(movie_id, frame_num, collection="s4_re_id")
+            matched_ids = []
+            for idx, post_processed_match in enumerate(post_processed_matches):
+                    face_id = str(post_processed_match['face_id'])
+                    vc_id = str(post_processed_match['vc_id'])
+
+                    if face_id in face_ids:
+                        face_ids.remove(face_id)
+                    if vc_id in vc_ids:
+                        vc_ids.remove(vc_id)
+
+                    matched_ids.append((face_id, vc_id))
+            unmatched_face_ids = face_ids
+            unmatched_vc_ids = vc_ids
+            data_for_db = {"movie_id": movie_id, "frame_num": str(frame_num), 'rois': [], 'face_ids_not_matched': unmatched_face_ids}
+            for idx in range(len(matched_ids)):
+                face_id = matched_ids[idx][0]
+                vc_id = matched_ids[idx][1]
+                data_for_db['rois'].append(
+                    {
+                        'face_id': face_id,
+                        'vc_id': vc_id,
+                        'reid_name': self.celebrity_data[str(face_id)]
+                    }
+                )  
+            for vc_id in unmatched_vc_ids:
+                data_for_db['rois'].append(
+                    {
+                        'face_id': "-1",
+                        'vc_id': vc_id
+                    }
+                )  
+            self.insert_json_to_db(data_for_db, collection_name="s4_fusion", key_list=['movie_id', 'frame_num'])
+
+        end_time = time.time() - start_time
+        print("Total time it took for fusion task: {}".format(end_time))
+        return True, None
 
     def get_celebrity_data(self):
         celebrity_dict = {}
@@ -88,17 +220,6 @@ class FusionPipeline:
             else:
                 input_type = pipeline_data["inputs"]["videoprocessing"]["movies"][0]["type"]
         return input_type
-    
-        
-    def run_fusion_pipeline(self, movie_id):
-        print("Starting to record time of visual clues!")
-        start_time = time.time()
-
-
-
-        end_time = time.time() - start_time
-        print("Total time it took for visual clues: {}".format(end_time))
-        return True, None
 
     def get_reid_detections(self, movie_id, collection):
         try:
@@ -307,6 +428,7 @@ class FusionPipeline:
 def main():
     
     fusion_pipeline = FusionPipeline()
+    fusion_pipeline.run_fusion_pipeline(movie_id="Movies/7023181708619934815")
     tag='v100'
     collection='pipelines'
     # movie_ids_v100 = fusion_pipeline.get_movie_ids_by_tag(tag, collection)
